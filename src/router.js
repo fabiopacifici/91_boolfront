260b821643f6d67208223b43cd7fcf51f74e6709
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-16 10:41:33 +0200

    add 404 route close#1

--- src/router.js
+++ src/router.js
@@ -1,36 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from './views/HomeView.vue';
-import AboutView from './views/AboutView.vue';
-import ContactsView from './views/ContactsView.vue';
-import SinglePostView from './views/SinglePostView.vue';
-
-
+import HomeView from "./views/HomeView.vue";
+import AboutView from "./views/AboutView.vue";
+import ContactsView from "./views/ContactsView.vue";
+import SinglePostView from "./views/SinglePostView.vue";
+import NotFound from "./views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     // All routes here
     {
-      'path' : '/',
-      'name' : 'home',
-      'component' : HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-     'path': '/blog/:slug',
-     'name': 'single-post',
-     'component': SinglePostView 
+      path: "/blog/:slug",
+      name: "single-post",
+      component: SinglePostView,
     },
     {
-      'path': '/about',
-      'name': 'about',
-      'component': AboutView
+      path: "/about",
+      name: "about",
+      component: AboutView,
     },
     {
-      'path': '/contacts',
-      'name': 'contacts',
-      'component': ContactsView
-    }
-  ]
-})
+      path: "/contacts",
+      name: "contacts",
+      component: ContactsView,
+    },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+  ],
+});
 
-export {router}
+export { router };
